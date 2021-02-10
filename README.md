bec5df55131d3d4ac0e2927ee87bad48ae5d6899
Ayushi96 <cs17mtech11007@iith.ac.in>
2021-02-10 15:44:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-tedghg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tedghg)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuildere.group({
+    name: '',
+    address: ''
+  });
+  
+  constructor(private cartService: CartService, 
+    private formBuildere: FormBuilder,
+  ) { }
+
+  onSubmit(): void{
+    this.items = this.cartService.clearCart();
+    console.warn("Your order has been submitted!", this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+
+    ngOnInit() {
+  }
+
+}
